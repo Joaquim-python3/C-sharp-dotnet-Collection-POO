0258b10abd4ebf6d22c78862975e5a7af7f7afce
Joaquim Cabral <joaquimcabral.2014@gmail.com>
2026-03-15 12:43:39 -0300

    Cria cliente e ajusta venda
    Cria:
    # Classe cliente
    
    Altera:
    # Classe venda para aceitar Cliente ou funcionario, para se adequar a funcionalidade de vender online ou presencial

--- Domain/Venda.cs
+++ Domain/Venda.cs
@@ -11,14 +11,31 @@ public class Venda
     public int id { get; set; }
     public DateTime DataVenda { get; set; }
     public List<Produto> Carrinho { get; set; }
+    public Cliente Cliente { get; set; }
+    public Funcionario Funcionario { get; set; }
+
     public Venda()
     {
     }
 
-    public Venda(int id, DateTime dataVenda, List<Produto> carrinho)
+// Venda presencial
+    public Venda(int id, DateTime dataVenda, List<Produto> carrinho, Funcionario funcionario)
     {
         this.id = id;
         DataVenda = dataVenda;
         Carrinho = carrinho;
+        Funcionario = funcionario;
     }
+
+// Venda online
+    public Venda(int id, DateTime dataVenda, Cliente cliente, List<Produto> carrinho, Funcionario funcionario)
+    {
+        this.id = id;
+        DataVenda = dataVenda;
+        Cliente = cliente;
+        Carrinho = carrinho;
+        Funcionario = funcionario;
+
+    }
+
 }
