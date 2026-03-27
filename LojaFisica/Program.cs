cf2fda08db636117766dc23f15d8226588ae1f7d
PedroLucas0102 <pedrolucas.sombra.moura@gmail.com>
2026-03-27 15:05:03 -0300

    Update Program.cs
    Chama o menu na main

--- LojaFisica/Program.cs
+++ LojaFisica/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LojaFisica;
+using Business;
+using Domain;
+
+Database db = new Database();
+LojaFisicaMenu menuLoja = new LojaFisicaMenu();
+ClienteRepository repo_cliente = new ClienteRepository(db);
+ProdutoRepository produtoRepo = new ProdutoRepository(db);
+VendaRepository vendaRepo = new VendaRepository(db);
+
+menuLoja.Menu(produtoRepo, vendaRepo, repo_cliente);
