8b5ff3e33ddd0cff07e1c3b4655c05b280c5efd4
Joaquim Cabral <joaquimcabral.2014@gmail.com>
2026-03-28 12:24:10 -0300

    Cria pasta FuncionarioService

--- Services/FuncionarioService.cs
+++ Services/FuncionarioService.cs
@@ -1,6 +1,15 @@
-namespace Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class FuncionarioService
+namespace Services
 {
-
+    public class FuncionarioService
+    {
+        public Funcionario AtualizarFuncionario(Funcionario funcionario)
+        {
+            
+        }
+    }
 }
